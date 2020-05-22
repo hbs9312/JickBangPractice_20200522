db2b452c191440418b95e3dcfeee60b8cf1b97a1
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-22 17:14:39 +0900

    길게 누르면 리스트에서 삭제 구현

--- app/src/main/java/kr/co/tjoeun/jickbangpractice_20200522/datas/DetailRoomActivity.java
+++ app/src/main/java/kr/co/tjoeun/jickbangpractice_20200522/datas/DetailRoomActivity.java
@@ -28,6 +28,7 @@ public class DetailRoomActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
         binding.dialBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

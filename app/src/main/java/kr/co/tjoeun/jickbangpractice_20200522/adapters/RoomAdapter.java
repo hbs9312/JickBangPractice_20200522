f8c64cdd48e0ebf6593f6b17c1a7a3f85b9b7fdc
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-22 15:09:19 +0900

    getView메소드 오버라이딩

--- app/src/main/java/kr/co/tjoeun/jickbangpractice_20200522/adapters/RoomAdapter.java
+++ app/src/main/java/kr/co/tjoeun/jickbangpractice_20200522/adapters/RoomAdapter.java
@@ -2,9 +2,12 @@ package kr.co.tjoeun.jickbangpractice_20200522.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -24,7 +27,17 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         mList = objects;
         inf = LayoutInflater.from(mContext);
 
-
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View row = convertView; // 돌려막기 혹은 새로 그릴지 판단
+        if (row == null) {
+            row = inf.inflate(R.layout.room_list_item, null);
+        }
+
+        return row;
+    }
 }
